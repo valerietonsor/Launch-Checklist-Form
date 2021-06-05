65c435ab963d1f0e96667d2b55838f14943ccb82
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-06-04 19:24:55 -0500

    first commit

--- script.js
+++ script.js
@@ -11,3 +11,35 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotInput = document.getElementById("pilotName"); 
+      let copilotInput = document.getElementById("copilotName"); 
+      let fuelInput = Number(document.getElementById("fuelLevel")); 
+      let cargoInput = Number(document.getElementById("cargoMass")); 
+
+      if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || cargoInput.value === "") {
+         alert("All fields are required!"); 
+         event.preventDefault();
+      }
+      // if (isNaN(fuelInput) || isNaN(cargoInput) || !isNaN(copilotInput) || !isNaN(pilotInput)){ 
+      //    alert("Make sure to enter valid information for each field!"); 
+      //    event.preventDefault(); 
+      // }
+
+      let pilotPrint = document.getElementById("pilotStatus"); 
+      let copilotPrint = document.getElementById("copilotStatus"); 
+      let fuelPrint = document.getElementById("fuelStatus"); 
+      let cargoPrint = document.getElementById("cargoStatus"); 
+      let print = document.getElementById("faultyItems"); 
+
+      // print.style.visibility = "visible";       
+      pilotPrint.innerHTML(`Pilot ${pilotInput.value} is ready.`)
+      copilotPrint.innerHTML("This is working.")
+   
+});
+});
+
+ 
